dbbfa467adae0bb73c4c376944f203d1f5c63d6b
Nilo Ferreira <nsmf.gnf@gmail.com>
2022-12-15 16:40:35 -0300

    test: create permission

---
+++ src/application/use-cases/permission/create-permission.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryPermissionsRepository } from '@test/repositories/in-memory-permissions-repository';
+import { PermissionNameUnique } from '../errors/permission-name-unique';
+import { CreatePermission } from './create-permission';
+
+describe('Create Permission', () => {
+  it('should be able create a permission', async () => {
+    const permissionsRepository = new InMemoryPermissionsRepository();
+    const createPermission = new CreatePermission(permissionsRepository);
+
+    const { permission } = await createPermission.execute({
+      description: 'description-fake',
+      name: 'name-fake',
+    });
+
+    expect(permissionsRepository.items).toHaveLength(1);
+    expect(permissionsRepository.items[0]).toEqual(permission);
+  });
+  it('should not be able create a permission with same name', async () => {
+    const permissionsRepository = new InMemoryPermissionsRepository();
+    const createPermission = new CreatePermission(permissionsRepository);
+
+    await createPermission.execute({
+      description: 'description-fake',
+      name: 'name-fake',
+    });
+
+    expect(() => {
+      return createPermission.execute({
+        description: 'description-fake',
+        name: 'name-fake',
+      });
+    }).rejects.toThrow(PermissionNameUnique);
+  });
+});
