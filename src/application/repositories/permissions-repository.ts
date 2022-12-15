045e05b5a00d3616cf07b545b72fe802948174bd
Nilo Ferreira <nsmf.gnf@gmail.com>
2022-12-15 14:57:45 -0300

    feat: permissions contract

---
+++ src/application/repositories/permissions-repository.ts
@@ -0,0 +1,6 @@
+import { Permission } from '@app/entities/permission/permission';
+
+export abstract class PermissionsRepository {
+  abstract create(permission: Permission): Promise<void>;
+  abstract save(permission: Permission): Promise<void>;
+}
