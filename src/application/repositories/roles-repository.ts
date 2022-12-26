368e66b83e282a71bf5354f01ccae5f3daf1f6eb
Nilo Ferreira <nsmf.gnf@gmail.com>
2022-12-26 10:46:07 -0300

    fix: find by id

--- src/application/repositories/roles-repository.ts
+++ src/application/repositories/roles-repository.ts
@@ -4,4 +4,5 @@ export abstract class RolesRepository {
   abstract create(role: Role): Promise<void>;
   abstract save(role: Role): Promise<void>;
   abstract findByName(name: string): Promise<Role | null>;
+  abstract findById(id: string): Promise<Role | null>;
 }
